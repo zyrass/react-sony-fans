b9b3fbf4cfb8c0bc66a89d61d580a1fb84af5f55
Alain GUILLON <zyrass@outlook.fr>
2022-10-29 11:28:27 +0200

    fix: bug menu ps4

--- src/App/App.js
+++ src/App/App.js
@@ -1,16 +1,16 @@
 import { Outlet } from "react-router-dom";
 import Header from "../components/Header/Header";
-// import Footer from "../components/Footer/Footer";
+import Footer from "../components/Footer/Footer";
 import styles from "./App.module.scss";
 
 function App() {
   return (
     <main className={`${styles.main}`}>
       <Header />
-      <div className="flex-fill p-50">
+      <div className="flex-fill p-20">
         <Outlet />
       </div>
-      {/* <Footer /> */}
+      <Footer />
     </main>
   );
 }
